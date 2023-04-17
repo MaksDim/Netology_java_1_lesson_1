0a00c583ae9af8fa99fc00705bd32a91f446ba5d
Максимов Дмитрий <demasig@yandex.ru>
2023-04-17 09:30:48 +0400

    first coomit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 500;
+        int passengersCount = 4;
+        int taxiPrice = 200;
+        int totalSum = ticketPrice * passengersCount + taxiPrice;
+        System.out.println(totalSum);
+    }
+}
